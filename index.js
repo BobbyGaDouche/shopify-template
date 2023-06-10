fb8568532df7ab930fd53c1629bcf54334f6c060
BobbyGaDouche <info@brewed.online>
2023-06-10 12:48:35 +0100

    Updated package.json with start script

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const Shopify = require('shopify-api-node');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const Shopify = require('shopify-api-node');
 const shopify = new Shopify({
-  shopName: 'your-shop-name',
+  shopName: process.env.SHOP_NAME,
   apiKey: process.env.SHOPIFY_API_KEY,
-  password: process.env.SHOPIFY_API_SECRET,
+  password: process.env.SHOPIFY_API_PASSWORD,
   apiVersion: process.env.SHOPIFY_API_VERSION,
 });
+
 
 app.get('/', (req, res) => {
   shopify.product

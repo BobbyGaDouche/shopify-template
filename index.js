57cbe1edb5ff1ae3ebdbe5279108314371059e9a
BobbyGaDouche <info@brewed.online>
2023-06-10 13:38:49 +0100

    Updated package.json with start script

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-// Import the necessary modules and functions
-import { shopifyApi } from '@shopify/shopify-api';
-import { ShopifyRestResources } from '@shopify/shopify-api/dist/rest/types';
+const { shopifyApi } = require('@shopify/shopify-api');
+const { ShopifyRestResources } = require('@shopify/shopify-api/dist/rest/types');
 
-// Define your config parameters
 const config = {
   apiKey: 'f407beb8cca59260ce936842917934cd',
   apiSecretKey: 'c537296e3cc78220350920f12d7c23d7',
-  scopes: ['read_products', 'write_orders'], // Example scopes: read_products, write_orders
-  hostName: 'brewedonline.myshopify.com',
+  scopes: ['read_products', 'write_orders'],
+  hostName: 'brewedonline.myshopify.com'
 };
 
-// Create the Shopify instance
-const shopify = shopifyApi<ShopifyRestResources>(config);
+const shopify = shopifyApi(config);
 
-// Use the Shopify instance
-// Example: Output the Shopify API library version
 console.log(`Shopify API Library Version: ${shopify.config.libraryVersion}`);
 
-// Example: Access the REST resource
 shopify.rest.products
   .list()
   .then((products) => {
